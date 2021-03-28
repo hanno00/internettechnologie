5e0baf694259dd491c83720263d910a6112bbe0a
Hanno Brandwijk <brandwijkh@gmail.com>
2021-03-28 20:19:09 +0200

    Add Navigation

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,27 +9,12 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  };
   heroes: Hero[] = [];
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   ngOnInit(): void {
     this.getHeroes();
-  }
-
-  selectedHero?: Hero;
-  onSelect(hero: Hero): void {
-    if (this.selectedHero != hero) {
-      this.selectedHero = hero;
-      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-    } else {
-      this.selectedHero = undefined;
-    }
-    
   }
 
   getHeroes(): void {
